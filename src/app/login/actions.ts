358ba98e4bc0caa32fd2bea93b7adb8ecdc2f789
Carlos Santos Expósito <147606893+Apagafuegos@users.noreply.github.com>
2025-12-25 13:57:29 +0100

    feat: Dynamically set signup email redirect URL using request headers.

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -2,6 +2,7 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { headers } from 'next/headers'
 import { createClient } from '@/utils/supabase/server'
 
 export async function login(formData: FormData) {
@@ -26,6 +27,7 @@ export async function login(formData: FormData) {
 
 export async function signup(formData: FormData) {
     const supabase = await createClient()
+    const origin = (await headers()).get('origin')
 
     const email = formData.get('email') as string
     const password = formData.get('password') as string
@@ -34,7 +36,7 @@ export async function signup(formData: FormData) {
         email,
         password,
         options: {
-            emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'}/auth/callback`,
+            emailRedirectTo: `${origin}/auth/callback`,
         },
     })
 
